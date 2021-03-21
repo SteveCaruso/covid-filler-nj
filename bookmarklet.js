d388aef63bce3e7da7bbc9dfb7d42c468e057c8b
Steve Caruso <steven.caruso@raritanval.edu>
2021-03-20 22:51:10 -0400

    Split files.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,72 +1,63 @@
-/*
+/* Data columns */
+var FNAME = 1;
+var LNAME = 2;
+var BDAY = 3;
+var PHONE = 5;
+var ADDR = 6;
+var CITY = 7;
+var STATE = 8;
+var ZIP = 9;
+var OCC = 10;
+var HEALTH = 11;
+var EMAIL = 15;
 
-This is to help sign folks up for CoVID vaccines from a variety of places.
+/* Construct button */
+var button = document.createElement("button");
+    button.id = "MakeTheMagicHappen";
+    button.innerHTML = "<h3>CoVID Filler is Loaded!</h3><br>Make sure the person's info is in your clipboard.<br/>Then click on me.";
+    button.style.position = "fixed";
+    button.style.left="10px";
+    button.style.top="10px";
+    button.style.border="3px solid red";
+    button.style.zIndex = 10000;
+    button.style.borderRadius = "5px";
+    button.style.boxShadow = "5px 5px 5px 5px rgba(0,0,0,.5)";
 
-*/
+/* Inject into document */
+document.body.appendChild(button);
 
-(function() {
+/* Add click function */
+button.onclick = function() {
 
-    function inject() {
+    var h = document.location.host;
+    var el = document.getElementById.bind(document);
 
-        //Construct button
-        var button = document.createElement("button");
-            button.id = "MakeTheMagicHappen";
-            button.innerHTML = `
-                <h3>CoVID Filler is Loaded!</h3><br>
-                Make sure the person's info is in your clipboard.<br/>
-                Then click on me.`;
-            button.style.position = "fixed";
-            button.style.left="10px";
-            button.style.top="10px";
-            button.style.border="3px solid red";
-            button.style.zIndex = 10000;
-            button.style.borderRadius = "5px";
-            button.style.boxShadow = "5px 5px 5px 5px rgba(0,0,0,.5)";
-        
-        //Inject into document
-        document.body.appendChild(button);
+    /* Clipboard stuff */
+    navigator.clipboard.readText().then( c => {
 
-        //Add click function
-        button.onclick = function() {
+        c = c.split("	");
+        var d = c[BDAY].split('/');
+            console.log(d);
+            d = d[0].padStart(2,"0")+'/'+d[1].padStart(2,"0")+'/'+d[2]; 
 
-            var h = document.location.host;
-            var el = document.getElementById.bind(document);
+        if (h == "www.riteaid.com") {
+            el("dateOfBirth").value=d;
+            el("city").value=c[CITY];
+            el("state").value=c[STATE];
+            el("eligibility_state").value=c[STATE];
+            el("zip").value=c[ZIP];
+            
+            el("Occupation").value=c[OCC];
+            el("occu").value=c[OCC];
 
-            //Clipboard stuff
-            navigator.clipboard.readText().then( c => {
+            el("mediconditions").value=c[HEALTH];
+            el("medcond").value=c[HEALTH];
 
-                c = c.split("	");
-                var d = c[3].split('/');
-                    console.log(d);
-                    d = d[0].padStart(2,"0")+'/'+d[1].padStart(2,"0")+'/'+d[2]; 
-
-                if (h == "www.riteaid.com") {
-                    el("dateOfBirth").value=d;
-                    el("city").value=c[7];
-                    el("state").value=c[8];
-                    el("eligibility_state").value=c[8];
-                    el("zip").value=c[9];
-                    
-                    el("Occupation").value=c[10];
-                    el("occu").value=c[10];
-
-                    el("mediconditions").value=c[11];
-                    el("medcond").value=c[11];
-
-                    el("continue").disabled = false;
-                    el("continue").click();
-
-                }
-
-            });
+            el("continue").disabled = false;
+            el("continue").click();
 
         }
 
-    }
+    });
 
-    //Place it in a global object
-    window.COVID = {
-        inject : inject;
-    };
-
-})();
+}
