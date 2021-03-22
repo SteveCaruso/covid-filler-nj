88c09bd50ffcfabba1d7d6ad4effb7b5b65e71e6
Steve Caruso <steven.caruso@raritanval.edu>
2021-03-21 21:27:07 -0400

    Add first draft of CVS for testing.

--- index.js
+++ index.js
@@ -8,15 +8,26 @@ It's a *snicker* JavaScript injection... :-)
 
 (function() {
 
-    function inject() {
+    async function inject() {
+
+        //Wait function
+        var wait = function (time) {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve();
+                }, time);
+            });
+        }
 
         //Shorten some commonly-used stuff
         var host = document.location.host;
         var el = document.getElementById.bind(document);
+        var q = document.querySelector.bind(document);
 
         //List of supported hosts
         var supported = [
-            "www.riteaid.com"
+            "www.riteaid.com",
+            "www.cvs.com"
         ];
 
         //Check to see if we're *on* a supported host, otherwise we shouldn't mess with it.
@@ -90,6 +101,176 @@ It's a *snicker* JavaScript injection... :-)
                     //Since there are no fields that need manual filling, click next
                     el("continue").disabled = false;
                     el("continue").click();
+
+                }
+                else if (host == "www.cvs.com") {
+
+                    //On: https://www.cvs.com/immunizations/covid-19-vaccine
+                    if (location.href == "https://www.cvs.com/immunizations/covid-19-vaccine") {
+                    
+                        //Click: a[data-analytics-name="New York"]
+                        q('a[data-analytics-name="New York"]').click();
+
+                        //wait
+                        wait(500);
+
+                        //Click: a[data-analytics-name="Schedule an appointment now"]
+                        q('a[data-analytics-name="Schedule an appointment now"]').click();
+
+                    }
+                    //On: https://www.cvs.com/vaccine/intake/store/covid-screener/covid-qns
+                    else if (location.href == "https://www.cvs.com/vaccine/intake/store/covid-screener/covid-qns") {
+                        //Click #q7_2
+                        q('#q7_2').click();
+                        //Click #q8_2
+                        q('#q8_2').click();
+                        //Click #q9_2
+                        q('#q9_2').click();
+
+                        //Click button[_ngcontent-hll-c70]
+                        q('button[_ngcontent-hll-c70]').click();
+
+                        //wait
+                        wait(500);
+
+                        //Click #customRadio_1
+                        q('#customRadio_1').click();
+
+                        //Click button[_ngcontent-kqo-c72][type="submit"]
+                        q('button[_ngcontent-kqo-c72][type="submit"]').click();
+                    }
+                    //On: https://www.cvs.com/vaccine/intake/store/eligibility-screener/eligibility-covid
+                    else if (location.href == "https://www.cvs.com/vaccine/intake/store/eligibility-screener/eligibility-covid") {
+
+                        //Set #jurisdiction 's selected element to one with value=
+                        //26: EID_NJ
+                        q('#jurisdiction').innerHTML = `<option value="26: EID_NJ" selected></option>`;
+
+                        //Click button! <--- Fix!
+
+                    }
+                    //On: https://www.cvs.com/vaccine/intake/store/eligibility-screener/eligibility-qns
+
+                        //Age in #q_01
+                        //Have to calculate it roughly
+                        var age = (new Date().getFullYear()) - parseInt(c[BDAY].split('/')[2]);
+                        q('#q_01').value = age;
+
+                        //Click #q20
+                        q('#q20').click();
+
+                        //wait
+                        wait(500);
+
+
+                        //Select elligibility criteria
+                        //THIS PART SUCKS
+                        /*
+
+                        <select _ngcontent-kqo-c76="" id="qlist" aria-describedby="listErr" formcontrolname="list" class="dd-list ng-pristine ng-invalid ng-touched">
+                            <option _ngcontent-kqo-c76="" disabled="" value="0: null">Select</option>
+                            <option _ngcontent-kqo-c76="" value="1: Age 65 or over"> Age 65 or over</option>
+                            <option _ngcontent-kqo-c76="" value="2: Teachers K-12, Daycare and preschool workers, a"> Teachers K-12, Daycare and preschool workers, and staff members</option>
+                            <option _ngcontent-kqo-c76="" value="3: Healthcare workers (paid and unpaid)"> Healthcare workers (paid and unpaid)</option>
+                            <option _ngcontent-kqo-c76="" value="4: Age 16 or over with medical conditions that inc"> Age 16 or over with medical conditions that increase risk of severe illness from COVID-19</option>
+                            <option _ngcontent-kqo-c76="" value="5: First responders including law enforcement and "> First responders including law enforcement and fire professionals</option>
+                            <option _ngcontent-kqo-c76="" value="6: Transit and public safety worker"> Transit and public safety worker</option>
+                            <option _ngcontent-kqo-c76="" value="7: Migrant Farm Worker"> Migrant Farm Worker</option>
+                            <option _ngcontent-kqo-c76="" value="8: Member of a tribal community"> Member of a tribal community</option>
+                            <option _ngcontent-kqo-c76="" value="9: Person experiencing homelessness or living in a"> Person experiencing homelessness or living in a shelter</option>
+                            <option _ngcontent-kqo-c76="" value="10: Residents of long-term and high risk congregat"> Residents of long-term and high risk congregate care settings</option>
+                            <option _ngcontent-kqo-c76="" value="11: Healthcare workers or staff of long-term and h"> Healthcare workers or staff of long-term and high risk congregate care settings</option>
+                            <option _ngcontent-kqo-c76="" value="12: None of the above"> None of the above</option>
+                        </select>
+
+                        */
+
+                        // For some selections #qtext needs to be given info
+                        // K-12, First Responders, Transit & PS, Farm Worker, Healthcare Workers
+                        // NEED employer 
+
+                        //Senior
+                        if (age >= 65) {
+                            q('option[value="1: Age 65 or over"]').setAttribute("selected", "true");
+                        }
+                        //Any health problem
+                        else if (c[HEALTH] != "") {
+                            q('option[value="4: Age 16 or over with medical conditions that inc"]').setAttribute("selected", "true");
+                        }
+                        //K-12 & Childcare
+                        else if (c[OCC] == "Childcare Worker" 
+                                || c[OCC] == "Pre-Kindergarten -12th grade Educator & Staff"
+                                || c[OCC] == "Family Childcare Providers"
+                                || c[OCC] == "Head Start and Early Head Start"
+                        ) {
+                            q('option[value="2: Teachers K-12, Daycare and preschool workers, a"]').setAttribute("selected", "true");
+                        }
+                        //Healthcare Workers
+                        else if (c[OCC] == "Health Care Worker") {
+                            q('option[value="3: Healthcare workers (paid and unpaid)"]').setAttribute("selected", "true");
+                        }
+                        //First Responders
+                        else if (c[OCC] == "Emergency Medical Services (EMS)"
+                                || c[OCC] == "Firefighters"
+                                || c[OCC] == "First Responder"
+                                || c[OCC] == "Law Enforcement"
+                        ) {
+                            q('option[value="5: First responders including law enforcement and "]').setAttribute("selected", "true");
+                        }
+                        //Transit & Public Safety
+                        else if (c[OCC] == "Local Transportation"
+                                || c[OCC] == "Public Health Employee"
+                                || c[OCC] == "Public Safety Workers"
+                                || c[OCC] == "Public Transit Workers"
+                        ) {
+                            q('option[value="6: Transit and public safety worker"]').setAttribute("selected", "true");
+                        }
+                        //Migrant Farm Workers
+                        else if (c[OCC] == "Migrant Farm Workers") {
+                            q('option[value="7: Migrant Farm Worker"]').setAttribute("selected", "true");
+                        }
+                        //Tribal
+                        else if (c[OCC] == "Members of tribal communities") {
+                            q('option[value="8: Member of a tribal community"]').setAttribute("selected", "true");
+                        }
+                        //Tribal
+                        else if (c[OCC] == "Persons experiencing homelessness"
+                                || c[OCC] == "Persons living in shelters"
+                        ) {
+                            q('option[value="9: Person experiencing homelessness or living in a"]').setAttribute("selected", "true");
+                        }
+                        //Long Term Care
+                        else if (c[OCC] == "Multigenerational Household"
+                        ) {
+                            q('option[value="10: Residents of long-term and high risk congregat"]').setAttribute("selected", "true");
+                        }
+                        //Long Term Care Staff
+                        else if (c[OCC] == "Long Term Care Facility Staff") {
+                            q('option[value="11: Healthcare workers or staff of long-term and h"]').setAttribute("selected", "true");
+                        }
+                        //OTHER
+                        else {
+                            q('option[value="12: None of the above"]').setAttribute("selected", "true");
+                        }
+
+                        //Add employer info !!! We don't collect this yet...
+                        //q('#qtext')
+
+                        //Click button[_ngcontent-kqo-c76].btn-control
+                        q('button[_ngcontent-kqo-c76].btn-control').click();
+
+                    //On: https://www.cvs.com/vaccine/intake/store/cvd/how-to-schedule
+
+                        //Click button[_ngcontent-kqo-c74].btn-control
+                        q('button[_ngcontent-kqo-c74].btn-control').click();
+
+                    //On: https://www.cvs.com/vaccine/intake/store/cvd-store-select/first-dose-select
+
+                        //Set #address to Zip Code
+                        q('#address').value = c[ZIP];
+
+                        //Click button[_ngcontent-kqo-c79]
+                        q('button[_ngcontent-kqo-c79]').click();
 
                 }
 
