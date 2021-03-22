7cb1e57b78467ed39421fa5ade9d4e8637a70fcf
Steve Caruso <steven.caruso@raritanval.edu>
2021-03-22 01:05:18 -0400

    Add page 8 to CVS.

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ It's a *snicker* JavaScript injection... :-)
                     Make sure the person's info is in your clipboard.<br>
                     Then click here.
                 </div>`;
+            button.style.fontFamily = "Arial";
             button.style.width="250px";
             button.style.position = "fixed";
             button.style.left="10px";
@@ -131,7 +132,7 @@ It's a *snicker* JavaScript injection... :-)
                     if (location.href == "https://www.cvs.com/immunizations/covid-19-vaccine") {
                         console.log("Page 1 detected...");
 
-                        q('#COVID-STATUS').innerHTML = '●○○○○○○<br>CVS Page 1 detected...';
+                        q('#COVID-STATUS').innerHTML = '●○○○○○○○<br>CVS Page 1 detected...';
                     
                         //Click: a[data-analytics-name="New York"]
                         q('a[data-analytics-name="New York"]').click();
@@ -147,7 +148,7 @@ It's a *snicker* JavaScript injection... :-)
                     else if (location.href == "https://www.cvs.com/vaccine/intake/store/covid-screener/covid-qns") {
                         console.log("Page 2 detected...");
 
-                        q('#COVID-STATUS').innerHTML = '●●○○○○○<br>On CVS Page 2.<br>Click again.';
+                        q('#COVID-STATUS').innerHTML = '●●○○○○○○<br>On CVS Page 2.<br>Click again.';
 
                         //Click #q7_2
                         q('#q7_2').click();
@@ -164,7 +165,7 @@ It's a *snicker* JavaScript injection... :-)
                     else if (location.href == "https://www.cvs.com/vaccine/intake/store/cvd/dose-select") {
                         console.log("Page 2.5 detected...");
 
-                        q('#COVID-STATUS').innerHTML = '●●●○○○○<br>On CVS Page 3.<br>Click again.';
+                        q('#COVID-STATUS').innerHTML = '●●●○○○○○<br>On CVS Page 3.<br>Click again.';
 
                         //Click #customRadio_1
                         q('#customRadio_1').click();
@@ -177,7 +178,7 @@ It's a *snicker* JavaScript injection... :-)
                     else if (location.href == "https://www.cvs.com/vaccine/intake/store/eligibility-screener/eligibility-covid") {
                         console.log("Page 3 detected...");
 
-                        q('#COVID-STATUS').innerHTML = '●●●●○○○<br>On CVS Page 4.<br>Click again.';
+                        q('#COVID-STATUS').innerHTML = '●●●●○○○○<br>On CVS Page 4.<br>Click again.';
 
                         //Set #jurisdiction 's selected element to one with value=
                         //26: EID_NJ
@@ -194,7 +195,7 @@ It's a *snicker* JavaScript injection... :-)
                     else if (location.href == "https://www.cvs.com/vaccine/intake/store/eligibility-screener/eligibility-qns") {
                         console.log("Page 4 detected...");
 
-                        q('#COVID-STATUS').innerHTML = '●●●●●○○<br>On CVS Page 5.<br>Click again.';
+                        q('#COVID-STATUS').innerHTML = '●●●●●○○○<br>On CVS Page 5.<br>Click again.';
 
                         //Age in #q1_0
                         //Have to calculate it roughly
@@ -325,7 +326,7 @@ It's a *snicker* JavaScript injection... :-)
                     else if (location.href == "https://www.cvs.com/vaccine/intake/store/cvd/how-to-schedule") {
                         console.log("Page 5 detected...");
 
-                        q('#COVID-STATUS').innerHTML = '●●●●●●○<br>On CVS Page 6.<br>Click again.';
+                        q('#COVID-STATUS').innerHTML = '●●●●●●○○<br>On CVS Page 6.<br>Click again.';
                         
                         //Click button[_ngcontent-kqo-c74].btn-control
                         q('button.btn-control').click();
@@ -344,7 +345,62 @@ It's a *snicker* JavaScript injection... :-)
                         //Click button[_ngcontent-kqo-c79]
                         q('button').click();
 
-                        q('#COVID-STATUS').innerHTML = '●●●●●●●<br>On CVS Page 7.<br>Continue manually.';
+                        q('#COVID-STATUS').innerHTML = '●●●●●●●○<br>On CVS Page 7.<br>Continue manually until the <em>Please Provide Details</em> screen. Then click here again.';
+                    }
+
+                    //Patient info enter
+                    else if (location.href == "https://www.cvs.com/vaccine/intake/store/patient-info") {
+
+                        q('#COVID-STATUS').innerHTML = '●●●●●●●●<br>On CVS Page 8.<br>Continue manually.';
+
+                        //#firstName
+                        q('#firstName').value = c[FNAME];
+                        q('#firstName').dispatchEvent(new Event('compositionend'));
+
+                        //#lastName
+                        q('#lastName').value = c[LNAME];
+                        q('#lastName').dispatchEvent(new Event('compositionend'));
+
+                        //#dob -- MMDDYYYY without slashes
+                        q('#dob').value = d.split("/").join("");
+                        q('#dob').dispatchEvent(new Event('compositionend'));
+
+                        //#customRadio_F checkbox female
+                        //#customRadio_M checkbox male
+
+                        //#address
+                        q('#address').value = c[ADDR];
+                        q('#address').dispatchEvent(new Event('compositionend'));
+
+                        //#addressLine2
+                        //Address 2?
+
+                        //#city
+                        q('#city').value = c[CITY];
+                        q('#city').dispatchEvent(new Event('compositionend'));
+
+                        //#state dropdown
+                        q('option[value="35: NJ"]').setAttribute("selected","true");
+                        q('#state').dispatchEvent(new Event('change'));
+
+                        /*
+
+                        <select _ngcontent-rmj-c55="" id="state" formcontrolname="state" autocomplete="off" aria-describedby="" class="ng-untouched ng-pristine ng-invalid"><option _ngcontent-rmj-c55="" disabled="" value="0: null">Select</option><option _ngcontent-rmj-c55="" value="1: AL" aria-label="AL"> AL</option><option _ngcontent-rmj-c55="" value="2: AK" aria-label="AK"> AK</option><option _ngcontent-rmj-c55="" value="3: AS" aria-label="AS"> AS</option><option _ngcontent-rmj-c55="" value="4: AZ" aria-label="AZ"> AZ</option><option _ngcontent-rmj-c55="" value="5: AR" aria-label="AR"> AR</option><option _ngcontent-rmj-c55="" value="6: CA" aria-label="CA"> CA</option><option _ngcontent-rmj-c55="" value="7: CO" aria-label="CO"> CO</option><option _ngcontent-rmj-c55="" value="8: CT" aria-label="CT"> CT</option><option _ngcontent-rmj-c55="" value="9: DE" aria-label="DE"> DE</option><option _ngcontent-rmj-c55="" value="10: DC" aria-label="DC"> DC</option><option _ngcontent-rmj-c55="" value="11: FM" aria-label="FM"> FM</option><option _ngcontent-rmj-c55="" value="12: FL" aria-label="FL"> FL</option><option _ngcontent-rmj-c55="" value="13: GA" aria-label="GA"> GA</option><option _ngcontent-rmj-c55="" value="14: GU" aria-label="GU"> GU</option><option _ngcontent-rmj-c55="" value="15: HI" aria-label="HI"> HI</option><option _ngcontent-rmj-c55="" value="16: ID" aria-label="ID"> ID</option><option _ngcontent-rmj-c55="" value="17: IL" aria-label="IL"> IL</option><option _ngcontent-rmj-c55="" value="18: IN" aria-label="IN"> IN</option><option _ngcontent-rmj-c55="" value="19: IA" aria-label="IA"> IA</option><option _ngcontent-rmj-c55="" value="20: KS" aria-label="KS"> KS</option><option _ngcontent-rmj-c55="" value="21: KY" aria-label="KY"> KY</option><option _ngcontent-rmj-c55="" value="22: LA" aria-label="LA"> LA</option><option _ngcontent-rmj-c55="" value="23: ME" aria-label="ME"> ME</option><option _ngcontent-rmj-c55="" value="24: MH" aria-label="MH"> MH</option><option _ngcontent-rmj-c55="" value="25: MD" aria-label="MD"> MD</option><option _ngcontent-rmj-c55="" value="26: MA" aria-label="MA"> MA</option><option _ngcontent-rmj-c55="" value="27: MI" aria-label="MI"> MI</option><option _ngcontent-rmj-c55="" value="28: MN" aria-label="MN"> MN</option><option _ngcontent-rmj-c55="" value="29: MS" aria-label="MS"> MS</option><option _ngcontent-rmj-c55="" value="30: MO" aria-label="MO"> MO</option><option _ngcontent-rmj-c55="" value="31: MT" aria-label="MT"> MT</option><option _ngcontent-rmj-c55="" value="32: NE" aria-label="NE"> NE</option><option _ngcontent-rmj-c55="" value="33: NV" aria-label="NV"> NV</option><option _ngcontent-rmj-c55="" value="34: NH" aria-label="NH"> NH</option><option _ngcontent-rmj-c55="" value="35: NJ" aria-label="NJ"> NJ</option><option _ngcontent-rmj-c55="" value="36: NM" aria-label="NM"> NM</option><option _ngcontent-rmj-c55="" value="37: NY" aria-label="NY"> NY</option><option _ngcontent-rmj-c55="" value="38: NC" aria-label="NC"> NC</option><option _ngcontent-rmj-c55="" value="39: ND" aria-label="ND"> ND</option><option _ngcontent-rmj-c55="" value="40: MP" aria-label="MP"> MP</option><option _ngcontent-rmj-c55="" value="41: OH" aria-label="OH"> OH</option><option _ngcontent-rmj-c55="" value="42: OK" aria-label="OK"> OK</option><option _ngcontent-rmj-c55="" value="43: OR" aria-label="OR"> OR</option><option _ngcontent-rmj-c55="" value="44: PW" aria-label="PW"> PW</option><option _ngcontent-rmj-c55="" value="45: PA" aria-label="PA"> PA</option><option _ngcontent-rmj-c55="" value="46: PR" aria-label="PR"> PR</option><option _ngcontent-rmj-c55="" value="47: RI" aria-label="RI"> RI</option><option _ngcontent-rmj-c55="" value="48: SC" aria-label="SC"> SC</option><option _ngcontent-rmj-c55="" value="49: SD" aria-label="SD"> SD</option><option _ngcontent-rmj-c55="" value="50: TN" aria-label="TN"> TN</option><option _ngcontent-rmj-c55="" value="51: TX" aria-label="TX"> TX</option><option _ngcontent-rmj-c55="" value="52: UT" aria-label="UT"> UT</option><option _ngcontent-rmj-c55="" value="53: VT" aria-label="VT"> VT</option><option _ngcontent-rmj-c55="" value="54: VI" aria-label="VI"> VI</option><option _ngcontent-rmj-c55="" value="55: VA" aria-label="VA"> VA</option><option _ngcontent-rmj-c55="" value="56: WA" aria-label="WA"> WA</option><option _ngcontent-rmj-c55="" value="57: WV" aria-label="WV"> WV</option><option _ngcontent-rmj-c55="" value="58: WI" aria-label="WI"> WI</option><option _ngcontent-rmj-c55="" value="59: WY" aria-label="WY"> WY</option><!----></select>
+
+                        */
+
+                        //#zip
+                        q('#zip').value = c[ZIP];
+                        q('#zip').dispatchEvent(new Event('compositionend'));
+
+                        //#email
+                        q('#email').value = c[EMAIL];
+                        q('#email').dispatchEvent(new Event('compositionend'));
+
+                        //#phoneNumber -- all digits
+                        q('#phone').value = c[PHONE].split("-").join("").split(" ").join("");
+                        q('#phone').dispatchEvent(new Event('compositionend'));
+
                     }
                 
                 } //END CVS
